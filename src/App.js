d8e7d5f8124cf63865a6d7dc80f65936670d5cf8
SHAHARYAR MALIK <shaharyar.malik2000@gmail.com>
2021-03-05 21:28:39 +0500

    third

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 import Home from './Home'
 import About from './About'
 import Users from './Users'
- import firebase from './firebase'
+//  import firebase from './firebase'
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 function App() {
   // React.useEffect(()=>{
